290bc231590d49d812aeb9903f1835763a23cb66
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2026-08-13 17:18:42 +0000

    Tag tests without retrieval.

--- src/main/java/troy/autofish/utils/RegistryUtils.java
+++ src/main/java/troy/autofish/utils/RegistryUtils.java
@@ -1,3 +1,6 @@
+// 2026 © Lumière Élevé
+// Parts of the code below are licensed under GNU LGPL 3.0+.
+
 package troy.autofish.utils;
 
 import java.util.HashMap;
@@ -11,14 +14,18 @@ import net.minecraft.resources.Identifier;
 //import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 //import net.minecraft.tags.BlockTags;
+//import net.minecraft.tags.EntityTypeTags;
 //import net.minecraft.tags.FluidTags;
 //import net.minecraft.tags.ItemTags;
-//import net.minecraft.tags.TagKey;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
-//import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
 //import troy.autofish.LogSession;
 
 public class RegistryUtils {
@@ -32,7 +39,7 @@ public class RegistryUtils {
 		return (TagKey<T>) source; // Unchecked cast!
 	}*/
 
-	/** Utility method for returning cached tags. Namespace "minecraft" and path "logs_that_burn" will be assembled into "minecraft:logs_that_burn" first to query the cache, before the actual identifier creation ever happens. */
+	/** Retrieve cached tags. Namespace "minecraft" and path "logs_that_burn" will be assembled into "minecraft:logs_that_burn" first to query the cache, before the actual identifier creation ever happens. */
 	public static Identifier getIdentifier(String namespace, String path) {
 		if (namespace == null || namespace.length() <= 0) {
 			namespace = "minecraft";
@@ -43,7 +50,7 @@ public class RegistryUtils {
 		fullIdCache.put(fullPath, targetId);
 		return targetId;
 	};
-	/** Utility method for returning cached tags. Recommended due to faster cache hits. */
+	/** Retrieve cached tags. Recommended due to faster cache hits. */
 	public static Identifier getIdentifier(String fullPath) {
 		if (!fullPath.contains(":")) {
 			fullPath = "minecraft:" + fullPath;
@@ -53,12 +60,12 @@ public class RegistryUtils {
 		fullIdCache.put(fullPath, targetId);
 		return targetId;
 	};
-	/** Utility method for returning tag containers. */
+	/** Retrieve tag containers. */
 	/*public static <T> Object getTagContainer(Registry<T> registry, TagKey<T> tagKey, Identifier id) {
 		Object tagContainer = registry.get(id);
 		return tagContainer;
 	}*/
-	/** Utility method for returning cached tag keys. */
+	/** Retrieve cached tag keys. */
 	/*public static <T> TagKey<T> getTagKey(ResourceKey<Registry<T>> registry, String fullPath) {
 		// Example registry: net.minecraft.core.registries.Registries.BLOCK
 		Map<String, TagKey<?>> registryCache = splitTagCache.get(registry);
@@ -72,25 +79,76 @@ public class RegistryUtils {
 		registryCache.put(fullPath, targetTag);
 		return targetTag;
 	};*/
-	/** Utility method for returning registry ID keys like "minecraft:stone". */
+	/** Retrieve registry ID keys like "minecraft:stone". */
 	public static String getIdKey(Block block) {
 		if (block == null) return null;
 		return BuiltInRegistries.BLOCK.getKey(block).toString();
 	}
-	/** Utility method for returning registry ID keys like "minecraft:stick". */
+	/** Retrieve registry ID keys like "minecraft:stone". */
+	public static String getIdKey(BlockState blockState) {
+		if (blockState == null) return null;
+		return getIdKey(blockState.getBlock());
+	}
+	/** Retrieve registry ID keys like "minecraft:horse". */
+	public static String getIdKey(Entity entity) {
+		if (entity == null) return null;
+		return getIdKey(entity.getType());
+	}
+	/** Retrieve registry ID keys like "minecraft:horse". */
+	public static String getIdKey(EntityType<? extends Entity> entityType) {
+		if (entityType == null) return null;
+		return BuiltInRegistries.ENTITY_TYPE.getKey(entityType).toString();
+	}
+	/** Retrieve registry ID keys like "minecraft:water". */
+	public static String getIdKey(Fluid fluid) {
+		if (fluid == null) return null;
+		return BuiltInRegistries.FLUID.getKey(fluid).toString();
+	}
+	/** Retrieve registry ID keys like "minecraft:water". */
+	public static String getIdKey(FluidState fluidState) {
+		if (fluidState == null) return null;
+		return getIdKey(fluidState.getType());
+	}
+	/** Retrieve registry ID keys like "minecraft:stick". */
 	public static String getIdKey(Item item) {
 		if (item == null) return null;
 		return BuiltInRegistries.ITEM.getKey(item).toString();
 	}
-	/** Utility method for returning registry ID keys like "minecraft:stick". */
+	/** Retrieve registry ID keys like "minecraft:stick". */
 	public static String getIdKey(ItemStack itemStack) {
 		if (itemStack == null || itemStack == ItemStack.EMPTY) return null;
-		Item item = itemStack.getItem();
-		return getIdKey(item);
+		return getIdKey(itemStack.getItem());
 	}
-	/** Utility method for returning registry ID keys like "minecraft:horse". */
-	public static String getIdKey(Entity entity) {
-		if (entity == null) return null;
-		return BuiltInRegistries.ENTITY_TYPE.getKey(entity.getType()).toString();
+	/** Test if a given target is in a tag. */
+	public static boolean isIn(TagKey<Block> blockTag, Block block) {
+		return isIn(blockTag, block.defaultBlockState());
+	}
+	/** Test if a given target is in a tag. */
+	public static boolean isIn(TagKey<Block> blockTag, BlockState blockState) {
+		return blockState.is(blockTag);
+	}
+	/** Test if a given target is in a tag. */
+	/*public static boolean isIn(TagKey<EntityType<? extends Entity>> entityTypeTag, Entity entity) {
+		return isIn(entityTypeTag, entity.getType());
+	}*/
+	/** Test if a given target is in a tag. */
+	/*public static boolean isIn(TagKey<EntityType<? extends Entity>> entityTypeTag, EntityType<? extends Entity> entityType) {
+		return entityType.is(entityTypeTag);
+	}*/
+	/** Test if a given target is in a tag. */
+	public static boolean isIn(TagKey<Fluid> fluidTag, Fluid fluid) {
+		return isIn(fluidTag, fluid.defaultFluidState());
+	}
+	/** Test if a given target is in a tag. */
+	public static boolean isIn(TagKey<Fluid> fluidTag, FluidState fluidState) {
+		return fluidState.is(fluidTag);
+	}
+	/** Test if a given target is in a tag. */
+	public static boolean isIn(TagKey<Item> itemTag, Item item) {
+		return isIn(itemTag, item.getDefaultInstance());
+	}
+	/** Test if a given target is in a tag. */
+	public static boolean isIn(TagKey<Item> itemTag, ItemStack itemStack) {
+		return itemStack.is(itemTag);
 	}
 }
--- src/main/java/troy/autofish/utils/TagStoreUtil.java
+++ src/main/java/troy/autofish/utils/TagStoreUtil.java
@@ -1,3 +1,6 @@
+// 2026 © Lumière Élevé
+// Parts of the code below are licensed under GNU LGPL 3.0+.
+
 package troy.autofish.utils;
 
 public class TagStoreUtil {}
